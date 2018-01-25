ab2cb3a38bc4691cfd7119f87573896e8333c0c5
Silajit Sil <silajit42@gmail.com>
2018-01-25 21:01:41 +0530

    Initial commit

---
+++ app/src/main/java/com/squareandcube/advancechatapplication/model/Friend.java
@@ -0,0 +1,8 @@
+package com.squareandcube.advancechatapplication.model;
+
+
+
+public class Friend extends User{
+    public String id;
+    public String idRoom;
+}
---
+++ app/src/main/java/com/squareandcube/advancechatapplication/model/Message.java
@@ -0,0 +1,10 @@
+package com.squareandcube.advancechatapplication.model;
+
+
+
+public class Message{
+    public String idSender;
+    public String idReceiver;
+    public String text;
+    public long timestamp;
+}
